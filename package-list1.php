81817c99d9e957be99aa63d847bac8157c78e7ec
varshini777 <93136838+varshini777@users.noreply.github.com>
2023-01-06 14:44:44 +0530

    Add files via upload

---
+++ package-list1.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>VIHAARI  | Package List</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<?php include('includes/header.php');?>
+<!--- banner ---->
+<div class="banner-3">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> VIHAARI- Package List</h1>
+	</div>
+</div>
+<!--- /banner ---->
+<!--- rooms ---->
+<div class="rooms">
+	<div class="container">
+		
+		<div class="room-bottom">
+			<h3>Package List</h3>
+			<form method="POST" action="#">
+				Enter place <input type="text" name="name" >
+				<input type="submit">
+</form>
+
+					
+<?php 
+$name=$_POST['name'];
+$sql = "SELECT * from tbltourpackages where PackageLocation='".$name."'";
+$query = $dbh->prepare($sql);
+$query->execute();
+$result=$query->fetchAll(PDO::FETCH_OBJ);
+//$result=mysqli_fetch_array($query);
+$cnt=1;
+?>
+<div class="rom-btm">
+				<div class="col-md-3 room-left wow fadeInLeft animated" data-wow-delay=".5s">
+                    <?php echo $result->PackageName;?>
+					<img src="admin/pacakgeimages/<?php echo htmlentities($result->PackageImage);?>" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-6 room-midle wow fadeInUp animated" data-wow-delay=".5s">
+					<h4>Package Name: <?php echo htmlentities($result->PackageName);?></h4>
+                    <h6>Package Type : <?php echo htmlentities($result->PackageType);?></h6>
+                    <p><b>Package Location :</b> <?php echo htmlentities($result->PackageLocation);?></p>
+                    <p><b>Features</b> <?php echo htmlentities($result->PackageFetures);?></p>
+				</div>
+				<div class="col-md-3 room-right wow fadeInRight animated" data-wow-delay=".5s">
+					<h5>INR <?php echo htmlentities($result->PackagePrice);?></h5>
+					<a href="package-details.php?pkgid=<?php echo htmlentities($result->PackageId);?>" class="view">Details</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+<?php  ?>
+</div>
+</div>
+</div>
+		
+</body>
+</html>
